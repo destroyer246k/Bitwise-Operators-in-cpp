53cb48ba1c0a4542cc685c0416bc16593fa8b994
Patel Vishwas <149381035+destroyer246k@users.noreply.github.com>
2025-07-17 08:31:46 +0530

    Add files via upload

---
+++ Exp4_2_BinarySet&Rest.cpp
@@ -0,0 +1,20 @@
+// Patel Vishwas
+// 24070123072
+// A3
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 345; //101011001
+    int bit_set,bit_reset,set_bit,reset_bit;
+    cout << "Enter Bit to be set:";
+    cin >> bit_set;
+    cout << "Enter Bit to be reset";
+    cin >> bit_reset;
+    set_bit = a|(1 << bit_set);
+    reset_bit = a&(~(1<< bit_reset));
+    cout << "Set bit "<<set_bit<<endl;
+    cout << "reset bit "<< reset_bit<<endl;
+   
+    return 0;
+}
